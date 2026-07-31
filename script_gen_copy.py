0ab5baad1882e27065078c5f35f1807c27d6d376
Aidan-Jared <AidanJared42@gmail.com>
2026-07-31 12:50:03 +1200

    fixing best.json to have correct xder args

---
+++ script_gen_copy.py
@@ -0,0 +1,69 @@
+import argparse
+
+from utils.best_args import best_args
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--severity", type=int, default=1)
+parser.add_argument("--poison_task", type=int, default=0)
+parser.add_argument("--pcp", type=float, default=0.5)
+parser.add_argument("--pp", type=float, default=0.5)
+parser.add_argument(
+    "--corruptions",
+    nargs="+",
+    default=["gaussian_blur"],
+    type=str,
+    help="The list of corruptions to apply to the poisoned samples, if not provided will apply no corruptions.",
+    # choices=corruption_dict.keys(),
+)
+
+if __name__ == "__main__":
+    args = vars(parser.parse_args())
+
+    datasets = [
+        "seq-cifar100",
+        "seq-tinyimg",
+    ]
+    models = ["xder"]
+    data_backbone = {
+        "seq-cifar100": ["reduced-resnet18", "resnet18-pnn"],
+        "seq-tinyimg": ["resnet18-7x7", "resnet18-7x7-pt"],
+    }
+    with open("args.txt", "a") as f:
+        for data in datasets:
+            params = best_args[data]
+            for model in models:
+                if model in params:
+                    param = params[model]
+                else:
+                    continue
+                for backbone in data_backbone[data]:
+                    for buffer in param.keys():
+                        output = (
+                            "--seed 42 "
+                            + " --runs 5 "
+                            + " --model_config base "
+                            + " --dataset "
+                            + data
+                            + " --model "
+                            + model
+                            + " --backbone "
+                            + backbone
+                            + " --severity "
+                            + str(args["severity"])
+                            + " --poison_task "
+                            + str(args["poison_task"])
+                            + " --pcp "
+                            + str(args["pcp"])
+                            + " --pp "
+                            + str(args["pp"])
+                            + " --corruptions "
+                            + " ".join(args["corruptions"])
+                        )
+                        if buffer > 0:
+                            output = output + f" --buffer_size {buffer} "
+                        if "mnistmlp-pnn" in backbone:
+                            output += f" --input_size {28 * 28} "
+                        for arg, value in param[buffer].items():
+                            output += f" --{arg} {value} "
+                        # print(output)
+                        f.write(output + "\n")
